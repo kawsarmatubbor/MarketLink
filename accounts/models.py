08136495cf7e330b2afb796d3e60b9ea3415f1c6
kawsarmatubbor <kawsarmatubbordec@gmail.com>
2026-01-12 12:26:50 +0600

    add custom user model and manager

---
+++ accounts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import CustomUserManager
+
+# Custom user
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICE = (
+        ('customer', 'Customer'),
+        ('vendor', 'Vendor'),
+        ('admin', 'Admin')
+    )
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICE, default='customer')
+    profile_picture = models.ImageField(upload_to="profile_picture/", null=True, blank=True)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.email}({self.role})"
